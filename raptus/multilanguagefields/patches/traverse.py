7237edade5ac388e215214b6c5eb14a10126e6d6
Sebastian Gottfried <sebastiangottfried@web.de>
2016-08-10 00:41:28 +0200

    Image Bobo Traverse
    Fixes the image fullscreen view

--- raptus/multilanguagefields/patches/traverse.py
+++ raptus/multilanguagefields/patches/traverse.py
@@ -4,16 +4,16 @@
 from raptus.multilanguagefields import LOG
 from raptus.multilanguagefields.interfaces import IMultilanguageField
 from Products.Archetypes.BaseObject import BaseObject
-def __bobo_traverse__(self, REQUEST, name):
-    """Transparent access to multilanguage image scales for
-       content types holding an multilanguage ImageField named
-       'image'
 
-       NO BLOBS
+def __bobo_traverse__(self, REQUEST, name):
+    """ helper to access multilanguage image scales the old way during
+        `unrestrictedTraverse` calls
+
+        the method to be patched is '__bobo_traverse__'
     """
     if name.startswith('image_') or name == 'image':
-        field = self.getField('image')
-        if not IMultilanguageField.providedBy(field):
+        field = self.getField(name.split('_')[0])
+        if not IMultilanguageField.providedBy(field) or not hasattr(REQUEST, 'get'):
             return BaseObject.__bobo_traverse__(self, REQUEST, name)
         last = REQUEST.get('ACTUAL_URL', '').endswith(name)
         fieldname, scale = name, None
@@ -25,31 +25,32 @@ def __bobo_traverse__(self, REQUEST, name):
             if '_' in name:
                 fieldname, scale = name.split('_', 1)
             if last and REQUEST.get('HTTP_USER_AGENT', False):
-                _scale = scale
-                if _scale is not None:
-                    _scale = '_'+str(_scale)
-                else:
-                    _scale = ''
-                REQUEST.RESPONSE.redirect(self.absolute_url()+'/'+fieldname+'___'+field._getCurrentLanguage(self)+'___'+_scale)
+                # begin own code
+                if scale in field.getAvailableSizes(self):
+                    # end own code
+                    _scale = scale
+                    if _scale is not None:
+                        _scale = '_'+str(_scale)
+                    else:
+                        _scale = ''
+                    REQUEST.RESPONSE.redirect(self.absolute_url()+'/'+fieldname+'___'+field._getCurrentLanguage(self)+'___'+_scale)
             lang = field._getCurrentLanguage(self)
         lang_before = field._v_lang
         try:
             field.setLanguage(lang)
+            handler = IImageScaleHandler(field, None)
             image = None
-            if scale:
-                if scale in field.getAvailableSizes(self):
-                    image = field.getScale(self, scale=scale)
-            else:
-                image = field.getScale(self)
+            if handler is not None:
+                try:
+                    image = handler.getScale(self, scale)
+                except AttributeError: # no image available, do not raise as there might be one available as a fallback
+                    pass
             if not image: # language fallback
                 defaultLang = field.getDefaultLang(self)
                 if defaultLang and not defaultLang == lang:
                     field.setLanguage(defaultLang)
-                    if scale:
-                        if scale in field.getAvailableSizes(self):
-                            image = field.getScale(self, scale=scale)
-                    else:
-                        image = field.getScale(self)
+                    if handler is not None:
+                        image = handler.getScale(self, scale)
                 if image is not None:
                     if last and REQUEST.get('HTTP_USER_AGENT', False):
                         _scale = scale
@@ -60,8 +61,7 @@ def __bobo_traverse__(self, REQUEST, name):
                         REQUEST.RESPONSE.redirect(self.absolute_url()+'/'+fieldname+'___'+defaultLang+'___'+_scale)
         finally:
             field.setLanguage(lang_before)
-        if image is not None and not isinstance(image, basestring):
-            # image might be None or '' for empty images
+        if image is not None:
             return image
     return BaseObject.__bobo_traverse__(self, REQUEST, name)
 
